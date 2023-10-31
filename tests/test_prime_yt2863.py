6cc624d393796e6acd0e236fda729396b09593fa
amandatyw0712 <yt2863@columbia.edu>
2023-10-31 15:24:01 -0400

    Add is_prime test

--- tests/test_prime_yt2863.py
+++ tests/test_prime_yt2863.py
@@ -1,1 +1,7 @@
-from prime_yt2863 import prime_yt2863
+from prime_yt2863 import is_prime
+
+def test_is_prime():
+    assert is_prime(2) == True
+    assert is_prime(4) == False
+    assert is_prime(17) == True
+    assert is_prime(10) == False
